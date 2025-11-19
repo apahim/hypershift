ea47caab4c294f2838d113bc5e3e7732f9f52abb
Amador Pahim <apahim@redhat.com>
2025-11-19 10:02:14 +0000

    feat(client): add GCP platform apply configurations
    Add generated client configurations and apply configurations for GCP
    platform types. Includes typed clients for GCPPlatformSpec,
    GCPWorkloadIdentityConfig, and related types to support programmatic
    management of GCP platform resources.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- client/applyconfiguration/utils.go
+++ client/applyconfiguration/utils.go
@@ -159,6 +159,10 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &hypershiftv1beta1.GCPPrivateServiceConnectStatusApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("GCPResourceReference"):
 		return &hypershiftv1beta1.GCPResourceReferenceApplyConfiguration{}
+	case v1beta1.SchemeGroupVersion.WithKind("GCPServiceAccountsRef"):
+		return &hypershiftv1beta1.GCPServiceAccountsRefApplyConfiguration{}
+	case v1beta1.SchemeGroupVersion.WithKind("GCPWorkloadIdentityConfig"):
+		return &hypershiftv1beta1.GCPWorkloadIdentityConfigApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("HostedCluster"):
 		return &hypershiftv1beta1.HostedClusterApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("HostedClusterSpec"):
